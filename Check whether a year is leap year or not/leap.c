b49a16edc3e51ad406f6485451c905353fd9c0f4
SINCY JOSEPH <sincyjoseph535@gmail.com>
2021-07-18 21:05:47 +0530

    Create leap.c

---
+++ Check whether a year is leap year or not/leap.c
@@ -0,0 +1,24 @@
+Problem statement:- Program to Check whether a year is leap year or not.
+
+Data requirement:-
+
+   Input Data:-Year
+
+  Output Data:-String Output
+
+Program in C
+Here is the source code of the C Program to Check whether a year is leap year or not.
+    
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("Enter the Distance in Kms:");
+    double d;
+    scanf("%lf",&d);
+    printf("Enter the Time in Hrs:");
+    double t;
+    scanf("%lf",&t);
+    double speed=d/t;
+    printf("Speed is % 0.2lf(Km/Hr)\n",speed);
+}    
