4f01b2a163464c304100e5e36ee73acb2ab78dbb
SINCY JOSEPH <sincyjoseph535@gmail.com>
2021-07-18 23:41:48 +0530

    Create non.c

---
+++ C program to find non repeating characters in a string/non.c
@@ -0,0 +1,36 @@
+Algorithm:
+        Initialize the variables.
+        Accept the input.
+        Initialize a for loop and terminate it at the end of string. 
+        This for loop will calculate the frequency of each character.
+        Print the characters having frequency one.
+          
+          
+#include<stdio.h>
+int main()
+{
+    //Initializing variables
+    char str[100];
+    int i;
+    int freq[256] = {0};
+    
+    //Accepting inputs
+    printf("Enter the string: ");
+    gets(str);
+    
+    //Calculating frequency of each character
+    for(i=0; str[i]!='\0'; i++)
+    {
+        freq[str[i]]++;
+    }
+    
+    printf("The non repeating characters are: ");
+    for(i=0; i<256; i++)
+    {
+        if(freq[i] == 1)//Finding unique characters and print them
+        {
+            printf(" %c", i);
+        }
+    }
+    return 0;
+}
